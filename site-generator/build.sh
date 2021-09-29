3b053172b55cd725abbca6de8fbc4df336927b50
Malte Isberner <malte@redhat.com>
2021-09-29 14:27:55 +0200

    add build.sh script

---
+++ site-generator/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+die() {
+	echo >&2 "$@"
+	exit 1
+}
+
+NODE_PATH="$(pwd)/node_modules"
+
+[[ -f package.json && -f yarn.lock && -d "$NODE_PATH" ]] || die "This must be run from within the project directory"
+
+[[ -z "$BUNDLE_PATH" ]] || die "No bundle path specified"
+[[ -z "$CONTENT_REPO" ]] || die "No content repo specified"
+
+playbook_yml="$(mktemp)"
+cat >"$playbook_yml" <<EOF
+site:
+  title: RHACS documentation
+  start_page: rhacs::index.adoc
+content:
+  sources:
+  - url: $CONTENT_REPO
+    branches: main
+    start_path: docs
+ui:
+  bundle:
+    url: $BUNDLE_PATH
+EOF
+
+export PATH="$(yarn bin):$PATH"
+
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr NODE_PATH="$NODE_PATH" antora --generator antora-site-generator-lunr "$playbook_yml"
